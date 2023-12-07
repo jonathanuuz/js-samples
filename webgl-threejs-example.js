2d3323cae383594878d31d7683b2c391e4dc8d18
Yokekadenz <122162670+jonathanuuz@users.noreply.github.com>
2023-12-07 18:16:22 +0300

    Create webgl-threejs-example.js

---
+++ webgl-threejs-example.js
@@ -0,0 +1,6 @@
+// Example of creating a scene and cube using Three.js
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
